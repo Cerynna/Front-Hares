c8b2ea2c851ef144ebca761cfc3633e16bd23c40
Jeremy MARTIN <cerynna@gmail.com>
2018-07-12 01:54:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hares - Test Front</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="logo-header">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="bloc-header">
+
+                    <i class="fa fa-phone"></i>
+
+                    <p>
+                        +33(0)4 00 00 00 00
+                    </p>
+                    <p>
+                        +33(0)4 00 00 00 00
+                    </p>
+
+
+                </div>
+
+            </div>
+            <div class="col-3">
+                <div class="bloc-header">
+
+                    <i class="fa fa-map-marker"></i>
+                    <p>
+                        14 Avenue de l'adresse<br/>
+                        69000 TASSIN LA DEMI LUNE
+                    </p>
+                </div>
+
+            </div>
+            <div class="col-2">
+                <div class="bloc-header">
+
+                    <button type="button" class="btn btn-outline-secondary">Connexion</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</header>
+<nav>
+    <ul class="nav container">
+        <li class="nav-item">
+            <a class="nav-link active" href="#"><i class="fa fa-home"></i> Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+    </ul>
+</nav>
+<section>
+    <div class="line">
+        <div class="container">
+            <div class="row">
+                <div class="col" data-aos="fade-right">
+                    <img src="img/image1.jpg" alt="" class="">
+                </div>
+                <div class="col">
+                    <h3>Lorem ipsum dolor sit amet,</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae cumque, qui quia
+                        quo quod recusandae rerum tempore tenetur voluptates. Cupiditate doloremque ducimus eligendi
+                        enim eum inventore natus officia quibusdam.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae cumque, qui quia
+                        quo quod recusandae rerum tempore tenetur voluptates. Cupiditate doloremque ducimus eligendi
+                        enim eum inventore natus officia quibusdam.
+                    </p>
+                    <div class="text-center">
+
+                        <button type="button" class="btn btn-outline-secondary">En Savoir Plus</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="line">
+        <div class="container">
+            <div class="row action">
+                <div class="col">
+                    <i class="fa fa-user"></i>
+                    <h4>
+                        Lorem ipsum dolor sit amet
+                    </h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cum error facere impedit
+                        ipsam ipsum iste, maxime minima, modi nesciunt officiis optio quaerat quia saepe, soluta.
+                        Consectetur provident quaerat rerum!
+                    </p>
+                </div>
+                <div class="col">
+                    <i class="fa fa-comment"></i>
+                    <h4>
+                        Lorem ipsum dolor sit amet
+                    </h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cum error facere impedit
+                        ipsam ipsum iste, maxime minima, modi nesciunt officiis optio quaerat quia saepe, soluta.
+                        Consectetur provident quaerat rerum!
+                    </p>
+                </div>
+                <div class="col">
+                    <i class="fa fa-check"></i>
+                    <h4>
+                        Lorem ipsum dolor sit amet
+                    </h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cum error facere impedit
+                        ipsam ipsum iste, maxime minima, modi nesciunt officiis optio quaerat quia saepe, soluta.
+                        Consectetur provident quaerat rerum!
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="line contact">
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-6">
+                    <form class="row">
+                        <div class="col-12">
+                            <h2>Nous Contacter</h2>
+                        </div>
+                        <div class="col-6">
+                            <label for="firstName">First Name</label>
+                            <input type="text" name="firstName" id="firstName">
+                        </div>
+                        <div class="col-6">
+                            <label for="lastName">last Name</label>
+                            <input type="text" name="lastName" id="lastName">
+                        </div>
+
+                        <div class="col-6">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email">
+                        </div>
+                        <div class="col-6">
+                            <label for="phone">Phone</label>
+                            <input type="text" name="phone" id="phone">
+                        </div>
+
+                        <div class="col-12">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message"></textarea>
+                        </div>
+                    </form>
+
+
+                </div>
+                <div class="col map">
+
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                            src="https://www.openstreetmap.org/export/embed.html?bbox=4.6830081939697275%2C45.73931563664794%2C4.826345443725587%2C45.79027007360134&amp;layer=mapnik&amp;marker=45.764798672765274%2C4.754676818847656"></iframe>
+
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</section>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <ul>
+                    <li><a href="">Mantion Légale</a></li>
+                    <li><a href="">Politique de confidentialité</a></li>
+                    <li>CGU</li>
+                </ul>
+            </div>
+            <div class="col-4"></div>
+            <div class="col-4"></div>
+            <div class="col-12">
+                © 2018 All Rights Reserved.
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+        crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+    AOS.init();
+</script>
+
+</body>
+</html>
